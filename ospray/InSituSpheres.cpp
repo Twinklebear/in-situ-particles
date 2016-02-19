9873b126b8ecf9666db9c88f351905a96d6389f4
Will Usher <willusher.life@gmail.com>
2016-02-19 15:44:24 -0700

    Don't actually pull

--- ospray/InSituSpheres.cpp
+++ ospray/InSituSpheres.cpp
@@ -178,31 +178,27 @@ namespace ospray {
 
 	  // Launch the thread to poll the sim if we haven't already
 #if OSP_IS_PULL_LOOP
-	  static bool poll_once = true;
-	  if (poll_once){
-		  std::cout << "ospray::InSituSpheres: launching background polling thread\n";
-		  auto sim_poller = std::thread([&]{ pollSimulation(); });
-		  sim_poller.detach();
-		  poll_once = false;
-	  } else {
-		  auto sim_poller = std::thread([&]{
-			  // Just lie that we've been updated on occasion
-			  while (true){
-				  std::cout << "ospray::InSituSpheres: Polling for new timestep after "
-					  << poll_delay << "s\n";
-				  const auto millis =
-					  std::chrono::milliseconds(static_cast<std::chrono::milliseconds::rep>(
-							  poll_delay * 1000.0));
-				  std::this_thread::sleep_for(millis);
-				  // Tell the render process the bounds of the geometry in the world
-				  // and that we're dirty and should be updated
-				  if (ospray::mpi::world.rank == 1){
-					  MPI_CALL(Send(&world_bounds, 6, MPI_FLOAT, 0, 1, ospray::mpi::world.comm));
-				  }
+	  std::cout << "ospray::InSituSpheres: launching background polling thread\n";
+	  auto sim_poller = std::thread([&]{ pollSimulation(); });
+#else
+	  auto sim_poller = std::thread([&]{
+		  // Just lie that we've been updated on occasion and see what happens
+		  while (true){
+			  std::cout << "ospray::InSituSpheres: Polling for new timestep after "
+				  << poll_delay << "s\n";
+			  const auto millis =
+				  std::chrono::milliseconds(static_cast<std::chrono::milliseconds::rep>(
+						  poll_delay * 1000.0));
+			  std::this_thread::sleep_for(millis);
+			  // Tell the render process the bounds of the geometry in the world
+			  // and that we're dirty and should be updated
+			  if (ospray::mpi::world.rank == 1){
+				  MPI_CALL(Send(&world_bounds, 6, MPI_FLOAT, 0, 1, ospray::mpi::world.comm));
 			  }
-		  });
-	  }
+		  }
+	  });
 #endif
+	  sim_poller.detach();
   }
   void InSituSpheres::pollSimulation(){
 	  std::cout << "ospray::InSituSpheres: Polling for new timestep after " << poll_delay << "s\n";
