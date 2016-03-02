7da2d61c482441f0ce1e652f670cfa6d495187cf
Will Usher <will@sci.utah.edu>
2016-03-02 11:57:06 -0600

    setup for uintah

--- testing_defines.h
+++ testing_defines.h
@@ -7,9 +7,9 @@
 // ospray/InSituParticles.cpp and sg/common/Integrator.cpp
 // AO of 0.0003 seems to work well for Josh's Uintah sim
 // Testing on the AO/ghost size for the single LAMMPS nanosphere with 0.003 radius
-//#define AO_OCCLUSION_DISTANCE 0.003f 
+#define AO_OCCLUSION_DISTANCE 0.0003f 
 // For the LAMMPS scaling tests I used 0.028
-#define AO_OCCLUSION_DISTANCE 0.028f
+//#define AO_OCCLUSION_DISTANCE 0.028f
 // Toggles if we use the renderer rank to color or not in ospray/InSituParticles.cpp
 #define USE_RENDER_RANK_ATTRIB 0
 // Effects clipping and ao bounds used in TraversePacket.ispc
@@ -20,7 +20,7 @@
 // Whether we should poll just once or loop to get new timesteps
 #define POLL_ONCE 0
 // If we should reset the camera position upon getting the new world bounds
-#define REPOSITION_CAMERA 1
+#define REPOSITION_CAMERA 0
 // If we want to print the total number of particles (including duplicated ghost ones)
 #define PRINT_FULL_PARTICLE_COUNT 1
 
